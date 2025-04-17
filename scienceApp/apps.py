0d4690476cf20b2ea604f2f22ad14b128260123c
小黄 <126998330+a135435@users.noreply.github.com>
2025-04-17 11:09:29 +0800

    添加scienceApp应用配置

---
+++ scienceApp/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class ScienceappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'scienceApp'
+    verbose_name = '制造科学应用'
